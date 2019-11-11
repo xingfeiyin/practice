75b2f8ad1959023ed0e03b29b64af0da7d451063
xingfeiyin <xingfeiyin@creditease.cn>
2019-11-11 10:48:39 +0800

    删除无用的

--- src/test/java/com/yinxf/demo/ExpotTests.java
+++ src/test/java/com/yinxf/demo/ExpotTests.java
@@ -1,18 +1,13 @@
 package com.yinxf.demo;
 
 
-import com.yinxf.demo.model.NsTemp;
 import com.yinxf.demo.service.NsTempService;
-import com.yinxf.demo.utils.SecurityUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.io.*;
-import java.util.List;
 
 
 @RunWith(SpringRunner.class)
@@ -26,46 +21,6 @@ public class ExpotTests {
     @Test
     public void testUserInfo() {
 
-//        OutputStream os = null;
-//        try {
-//            File file = new File("D:/宜人贷.txt");
-//            os = new FileOutputStream(file);
-//
-//            System.out.println("开始导出文件。。。。");
-//            List<NsTemp> temps = null;
-//            int page = 1;
-//            int count = 10000;
-//            int num = 0;
-//            while (true){
-//                int pageNum = (page - 1) * count;
-//                temps = nsTempService.selectTempList(pageNum,count);
-//                if (temps == null || temps.size() == 0){
-//                    break;
-//                }
-//                StringBuilder sb = new StringBuilder();
-//                for (NsTemp temp:temps) {
-//                    sb.append(temp.getUserId()).append(",");
-////                    sb.append(SecurityUtils.decrypt(temp.getCertificateId())).append(",");
-//                    sb.append(temp.getModifyTime()).append("\r\n");
-//                }
-//                os.write(sb.toString().getBytes());
-//                os.flush();
-//                page++;
-//                num += temps.size();
-//                System.out.println("已导出数据："+num);
-//            }
-//            System.out.println("导出完成！！！！");
-//        }catch (Exception e){
-//            e.printStackTrace();
-//        }finally {
-//            if (os != null){
-//                try {
-//                    os.close();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        }
 
     }
 
